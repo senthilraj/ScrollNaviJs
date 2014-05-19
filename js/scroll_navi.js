991e37eff696e4b56de7767f226649f8ed20cf2a
senthil rajan <senthil2rajan@gmail.com>
2014-05-19 19:57:37 +0530

    Update scroll_navi.js

--- js/scroll_navi.js
+++ js/scroll_navi.js
@@ -24,10 +24,17 @@
             elem.css("top",wh+"px");
             
             $(elem_a).click(function(event){
+				var link_outerpage = $(this).attr("data-outerpage");
+				if(link_outerpage=='true')
+				{
+					console.log("this is outerpage link");	
+				}
+				else{
                 event.preventDefault();
-                var link_add = $(this).attr("href");
-                var pos = $(link_add).offset().top;
-                $('html, body').animate({scrollTop: pos}, settings.speed);
+					var link_add = $(this).attr("href");
+					var pos = $(link_add).offset().top;
+					$('html, body').animate({scrollTop: pos}, settings.speed);
+				}
             });
             
             win.scroll(function() {
